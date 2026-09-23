bf4efd39b9990cd2ca730c4e708642f6c9a4b56d
Cristobal Guerrero Diaz <cristobal10cisco@gmail.com>
2026-09-24 00:11:37 +0200

    Añadidos scripts y configuración de LAMP

---
+++ scripts/02_configure_lamp.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# scripts/02_configure_lamp.sh
+
+
+# Configure LAMP stack services and test page
+set -xeu
+chown -R www-data:www-data /var/www/html 
+chmod -R 755 /var/www/html
+cp -vf /vagrant/files/info.php /var/www/html/info.php
+systemctl enable --now apache2
+systemctl enable --now mariadb
